803bb760cc91e88b12036a6062521946c2145773
James Halliday <mail@substack.net>
2016-07-18 13:26:52 -0700

    strings and nested tags

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var through = require('through2')
 var sprintf = require('sprintf')
+var xtend = require('xtend')
 
 var BEGIN = 1, TYPE = 2, LEN = 3, DATA = 4, END = 5
 var SIGNED = 10
@@ -24,6 +25,8 @@ module.exports = function () {
   var size = 0
   var data = null
   var prev = null
+  var strings = []
+  var strpair = []
   return through.obj(write)
 
   function write (buf, enc, next) {
@@ -33,7 +36,7 @@ module.exports = function () {
         next(new Error('first byte in frame, expected: 0xff, got ' + show(b)))
       } else if (state === BEGIN) {
         state = TYPE
-      } else if (state === TYPE && b === 0xff) {
+      } else if (state === TYPE && b === 0xff) { // reset
         state = TYPE
         prev = null
       } else if (state === TYPE) {
@@ -69,26 +72,132 @@ module.exports = function () {
   }
   function show (n) { return sprintf('0x%02x', n) }
   function flush (stream, buf, data) {
-    if (data.type === 'node') {
-      var field = 0
-      var value = 0, npow = 1
+    var field = 0, value = 0, npow = 1, strpos = 0
+    if (data.type === 'timestamp') {
       for (var i = 0; i < buf.length; i++) {
         var b = buf[i]
-        if (field === 0) { // id
-          value += (b & (npow === 1 ? 0x7e : 0x7f)) * npow
-          if (npow === 1 && (b & 0x1 === 1)) value *= -1
-          npow *= 128
-          if (b < 0x80) {
-            if (prev) value += prev.id
-            npow = 1
-            data.id = value
-            field++
-          }
+        if (field === 0) signed('time')
+      }
+    } else if (data.type === 'node') {
+      for (var i = 0; i < buf.length; i++) {
+        var b = buf[i]
+        if (field === 0) signedDelta('id')
+        else if (field === 1 && b === 0x00) {
+          field = 5
+        } else if (field === 1) {
+          unsigned('version')
+        } else if (field === 2) {
+          signedDelta('timestamp')
+        } else if (field === 3 && data.timestamp === 0) {
+          field = 5
+          i--
+        } else if (field === 3) {
+          signedDelta('changeset')
+        } else if (field === 4) {
+          stringPair('uid','user')
+        } else if (field === 5) {
+          signedDelta('longitude')
+        } else if (field === 6) {
+          signedDelta('latitude')
+        } else if (field > 6) {
+          stringPair('_kv','tags')
         }
       }
-      prev = data
     }
-    console.log(buf)
-    stream.push(data)
+    prev = data
+    if (data.type === 'node') {
+      stream.push(xtend(data, {
+        latitude: data.latitude * 1e-7 - 90,
+        longitude: data.longitude * 1e-7 - 180
+      }))
+    } else stream.push(data)
+
+    function signedDelta (name) {
+      value += (b & (npow === 1 ? 0x7e : 0x7f)) * npow
+      if (npow === 1 && (b & 0x1 === 1)) value *= -1
+      npow *= 128
+      if (b < 0x80) {
+        if (prev && prev[name] !== undefined) value += prev[name]
+        npow = 1
+        data[name] = value
+        value = 0
+        field++
+      }
+    }
+    function signed (name) {
+      value += (b & (npow === 1 ? 0x7e : 0x7f)) * npow
+      npow *= 128
+      if (b < 0x80) {
+        data[name] = value
+        npow = 1
+        value = 0
+        field++
+      }
+    }
+    function unsigned (name) {
+      value += (b & 0x7f) * npow
+      npow *= 128
+      if (b < 0x80) {
+        npow = 1
+        data[name] = value
+        value = 0
+        field++
+      }
+    }
+    function stringPair (name0, name1) {
+      if (strpos === 0 && b === 0x00) {
+        strpos++
+      } else if (strpos === 0) {
+        value += (b & 0x7f) * npow
+        npow *= 128
+        if (b < 0x80) {
+          strpair[0] = strings[value-1][0]
+          strpair[1] = strings[value-1][1]
+          finish()
+        }
+      } else if (strpos === 1) {
+        if (name0 === 'uid') {
+          value = 0
+          npow = 1
+          for (var j = i+1; j < buf.length; j++) {
+            if (buf[j] === 0x00) break
+            value += buf[j] * npow
+            npow *= 256
+          }
+          strpair[0] = value
+          value = 0
+          npow = 1
+        } else {
+          for (var j = i+1; j < buf.length; j++) {
+            if (buf[j] === 0x00) break
+          }
+          strpair[0] = buf.slice(i, j).toString('utf8')
+        }
+        i = j
+        strpos++
+      } else if (strpos === 2) {
+        for (var j = i+1; j < buf.length; j++) {
+          if (buf[j] === 0x00) break
+        }
+        strpair[1] = buf.slice(i, j).toString('utf8')
+        strings.unshift([strpair[0],strpair[1]])
+        i = j
+        finish()
+      }
+
+      function finish () {
+        if (name0 === '_kv') {
+          if (!data[name1]) data[name1] = {}
+          data[name1][strpair[0]] = strpair[1]
+        } else {
+          data[name0] = strpair[0]
+          data[name1] = strpair[1]
+        }
+        value = 0
+        npow = 1
+        strpos = 0
+        field++
+      }
+    }
   }
 }
