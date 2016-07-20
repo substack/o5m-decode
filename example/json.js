e3e9585102877d4ba6029fb4b862d1a22bd32c4f
James Halliday <mail@substack.net>
2016-07-19 21:45:56 -0700

    json example

---
+++ example/json.js
@@ -0,0 +1,15 @@
+var through = require('through2')
+var decode = require('../')()
+process.stdin.pipe(decode).pipe(json()).pipe(process.stdout)
+
+function json () {
+  var i = 0
+  return through.obj(write, end)
+  function write (row, enc, next) {
+    next(null, (i++ === 0 ? ',' : '[') + JSON.stringify(row))
+  }
+  function end (next) {
+    this.push(']')
+    next()
+  }
+}
